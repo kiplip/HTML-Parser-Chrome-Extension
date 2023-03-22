d331073707b545efd1015f12dc200a3a72c1dae7
Adnan <kiplip@hotmail.com>
2023-03-22 06:11:12 +0500

    remove reparse button

--- popup.js
+++ popup.js
@@ -211,11 +211,7 @@ function createHtmlElement(element) {
      resultsDiv.innerHTML = "";
    });
   
-  document.getElementById("reparseBtn").addEventListener("click", () => {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, {action: "getSource"}, parseHtml);
-    });
-  });
+ 
 
  
 
